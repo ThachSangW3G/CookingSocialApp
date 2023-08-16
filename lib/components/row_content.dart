0655ef968b20c1e8a4481a95efe419ccbc7745ba
trungtinhn <trungtinh1620@gmail.com>
2023-08-16 17:01:26 +0700

    update account screen

---
+++ lib/components/row_content.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class RowContent extends StatelessWidget {
+  final String label;
+  final VoidCallback onTap;
+  const RowContent({super.key, required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 23,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                label,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  fontFamily: "CeraPro",
+                ),
+              ),
+              const Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.grey,
+                size: 30,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/constants/app_styles.dart
+++ lib/constants/app_styles.dart
@@ -5,10 +5,8 @@ const kActiveCardColour = Color(0xFF1D1E33);
 const kInactiveCardColour = Color(0xFF111328);
 const kBottomContainerColour = Color(0xFFEB1555);
 
-const kLabelTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color(0xFF8D8E98),
-);
+const kLabelTextStyle =
+    TextStyle(color: Colors.grey, fontFamily: "CeraPro", fontSize: 15);
 
 const kNumberTextStyle = TextStyle(
   fontSize: 50.0,
