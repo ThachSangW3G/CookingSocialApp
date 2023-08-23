d6a388e01d11b6b6fd3b53965a2242f082724b0b
ngochan <20520484@gm,uit.edu.vn>
2023-08-23 18:42:12 +0700

    create screen Add Grocery

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'package:cooking_social_app/screens/authentication/login_screen.dart';
-import 'package:cooking_social_app/screens/recipe/recipe_edit_screen.dart';
+import 'package:cooking_social_app/screens/recipe/add_grocery_screen.dart';
+// import 'package:cooking_social_app/screens/recipe/recipe_edit_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
         useMaterial3: true,
       ),
-      home: const RecipeEditScreen(),
+      home: const AddGroceryScreen(),
     );
   }
 }
