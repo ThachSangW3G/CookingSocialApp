7c2c54dd4066372173c4cfa9f0d62aa7fd00627a
trungtinhn <trungtinh1620@gmail.com>
2023-09-07 00:02:29 +0700

    update model recipe

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -8,12 +8,12 @@ class Recipe {
   bool isPublic;
   List<String> material;
   int numberLike;
-  int numberView;
+  int numberReView;
   int serves;
   String source;
   List<String> spice;
   List<String> steps;
-
+  String uidUser;
   Recipe(
       {required this.key,
       required this.url,
@@ -24,10 +24,48 @@ class Recipe {
       required this.isPublic,
       required this.material,
       required this.numberLike,
-      required this.numberView,
+      required this.numberReView,
       required this.serves,
       required this.source,
       required this.spice,
-      required this.steps
-    });
+      required this.steps,
+      required this.uidUser});
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+        key: json['key'],
+        url: json['url'],
+        name: json['name'],
+        cookTime: json['cookTime'],
+        description: json['description'],
+        difficult: json['difficult'],
+        isPublic: json['isPublic'],
+        material: List<String>.from(json['material']),
+        numberLike: json['numberLike'],
+        numberReView: json['numberView'],
+        serves: json['serves'],
+        source: json['source'],
+        spice: List<String>.from(json['spice']),
+        steps: List<String>.from(json['steps']),
+        uidUser: json['uidUser']);
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'key': key,
+      'URL': url,
+      'name': name,
+      'cookTime': cookTime,
+      'description': description,
+      'difficult': difficult,
+      'isPublic': isPublic,
+      'material': material,
+      'numberLike': numberLike,
+      'numberReView': numberReView,
+      'serves': serves,
+      'source': source,
+      'spice': spice,
+      'steps': steps,
+      'uidUser': uidUser
+    };
+  }
 }
