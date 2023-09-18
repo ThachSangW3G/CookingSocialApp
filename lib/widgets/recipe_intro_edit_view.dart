bb6189de90ce25ca0300d43d7da7523c3a64f273
ngochan <20520484@gm,uit.edu.vn>
2023-09-18 20:28:55 +0700

    update Recipe Edit UI

--- lib/widgets/recipe_intro_edit_view.dart
+++ lib/widgets/recipe_intro_edit_view.dart
@@ -3,6 +3,7 @@ import 'package:cooking_social_app/widgets/yes_no_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 enum Difficulty { Difficult, Medium, Easy }
 
@@ -11,18 +12,27 @@ class RecipeIntroEdit extends StatefulWidget {
 
   @override
   State<RecipeIntroEdit> createState() => _RecipeIntroEditState();
-
 }
 
 class _RecipeIntroEditState extends State<RecipeIntroEdit> {
   Difficulty selectedDifficulty = Difficulty.Difficult;
 
+  PickedFile? _pickedImage;
+  
+  Future<void> _pickImageFromGallery() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _pickedImage = pickedImage as PickedFile?;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
         child: Container(
-      padding:
-          const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 32),
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 32),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -54,16 +64,14 @@ class _RecipeIntroEditState extends State<RecipeIntroEdit> {
               Expanded(
                 child: TextField(
                   decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 0.0, horizontal: 0.0),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
                       labelText: 'minutes',
                       labelStyle: TextStyle(
                           fontFamily: 'CeraPro',
                           // fontSize: 14,
                           fontWeight: FontWeight.w400)),
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 ),
               ),
               const SizedBox(
@@ -72,13 +80,11 @@ class _RecipeIntroEditState extends State<RecipeIntroEdit> {
               Expanded(
                 child: TextField(
                   decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(
-                        vertical: 0.0, horizontal: 0.0),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
                     labelText: 'hours',
                   ),
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 ),
               ),
             ],
@@ -88,14 +94,41 @@ class _RecipeIntroEditState extends State<RecipeIntroEdit> {
           const SizedBox(
             height: 24,
           ),
-          ClipRRect(
-            child: Image.asset(
-              'assets/images/defaut_food.png',
-              width: MediaQuery.of(context).size.width,
-              height: 208,
-              fit: BoxFit.cover,
+          Stack(
+          children: [
+            ClipRRect(
+              child: Image.asset(
+                'assets/images/image_background.png',
+                width: MediaQuery.of(context).size.width,
+                height: 208,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
+            Positioned(
+              top: 16.0,
+              right: 16.0,
+              child: InkWell(                
+                onTap: _pickImageFromGallery,
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    // shape: BoxShape.rectangle,                  
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.white,
+                  ),
+                  child: SvgPicture.asset(
+                    'assets/icon_svg/pencil.svg',
+                    color: Colors.grey[800],
+                    height: 10,
+                    width: 10,
+                  ),
+                ),
+              ),
+            ),
+
+          ]),
 
           // DESCRIPTION -------------------------------
           const SizedBox(
@@ -110,9 +143,7 @@ class _RecipeIntroEditState extends State<RecipeIntroEdit> {
                     fontFamily: 'CeraPro',
                     // fontSize: 14,
                     fontWeight: FontWeight.w400)),
-            inputFormatters: [
-              FilteringTextInputFormatter.singleLineFormatter
-            ],
+            inputFormatters: [FilteringTextInputFormatter.singleLineFormatter],
             maxLines: null, // Cho phép hiển thị nhiều dòng văn bản
             keyboardType:
                 TextInputType.multiline, // Bàn phím hiển thị dạng đa dòng
@@ -209,13 +240,11 @@ class _RecipeIntroEditState extends State<RecipeIntroEdit> {
               Expanded(
                 child: TextField(
                   decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(
-                        vertical: 0.0, horizontal: 0.0),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
                     labelText: 'Serve',
                   ),
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 ),
               ),
             ],
@@ -241,7 +270,9 @@ class _RecipeIntroEditState extends State<RecipeIntroEdit> {
                     width: 180,
                   ),
                   Expanded(child: YesNoSwitch()),
-                  SizedBox(width: 20,)
+                  SizedBox(
+                    width: 20,
+                  )
                 ],
               )),
             ],
